b4998916bedff5e68206753d109fe77d1598f83d
Omer Yehudai <omer.yehudai@quadream.io>
2019-12-16 13:29:11 +0200

    sh*t fixes

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+CTFSPATH=${CTFSPATH:-$HOME/ctfs}
 IMAGE=researchbox
 TAG=latest
-CTFSPATH=${HOME}/ctfs
 NAME=ctf
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-h] [-t TAG] [ -p CTFSPATH]
+Usage: ${0##*/} [-h] [-t TAG] [-p CTFSPATH] [-n NAME]
 Run researchbox container and mount a local CTFs data path.
 
-    -h            display this help and exit.
-    -t DOCKERTAG  set docket tag (default: ${TAG}).
-    -p CTFSPATH   local path to mount to (default: ${CTFSPATH}).
-    -n NAME       set the container name (default: ${NAME})
+    -h            Display this help and exit
+    -t DOCKERTAG  Set docket tag (default: "${TAG}")
+    -p CTFSPATH   Local path to mount to (default: "${CTFSPATH}") (override default value by setting CTFSPATH env var)
+    -n NAME       Set the container name (default: "${NAME}")
 EOF
 }
 
